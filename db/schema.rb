10a7634fcf4085de29379915536b09318a1c006a
Guillaume Le Denmat <ledenmatg@gmail.com>
2018-08-23 14:38:42 +0200

    4.2 bis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_18_135407) do
+ActiveRecord::Schema.define(version: 2018_08_21_100058) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_08_18_135407) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
