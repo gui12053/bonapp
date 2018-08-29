0242376045dfa0e3864558cc980ee5827bce1134
Guillaume Le Denmat <ledenmatg@gmail.com>
2018-08-29 14:33:28 +0200

    british spelling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_141635) do
+ActiveRecord::Schema.define(version: 2018_08_24_191502) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
