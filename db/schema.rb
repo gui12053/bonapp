b2dbcaf2b3bcb5707b4872ded32dea96108755f8
Guillaume Le Denmat <ledenmatg@gmail.com>
2018-09-01 18:16:27 +0200

    4.4 orders.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_090941) do
+ActiveRecord::Schema.define(version: 2018_08_31_140906) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
