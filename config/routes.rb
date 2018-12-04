bd352235b9c5e46f70070e4e5d32af62732a832c
Guillaume Le Denmat <ledenmatg@gmail.com>
2018-12-04 15:27:22 +0100

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#landing_page'
+  # redirect your users to a different page
+  post 'simple_pages/thank_you'
+  # rerouting the landing page
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
