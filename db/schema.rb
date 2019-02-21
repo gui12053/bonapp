e19407a1848c6fb88490a48f937638d6b1607d03
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-02-21 14:49:07 +0100

    styling contact page and new product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_140906) do
+ActiveRecord::Schema.define(version: 2019_02_19_011314) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2018_08_31_140906) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.integer "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firt_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
