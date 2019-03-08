7155c10e70c25a2b4f61dc50eb2aaca04d6869bc
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-03-08 07:50:45 +0100

    5.5.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registration: "user_registrations" }
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
