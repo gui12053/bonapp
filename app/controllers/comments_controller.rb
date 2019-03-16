df572a3b0e79bc03b3d9908081fd390309e17b0e
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-03-16 14:24:02 +0100

    5.9.3 comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,30 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to product_path(@product)
+    @user = current_user
+
     respond_to do |format|
       if @comment.save
+        # ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
-      else
+        format.js
+       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+       end
+     end
+   end
+
+   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
+    end
 end
