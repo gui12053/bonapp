ecd09884d3217d5798f460e97be8b6092f85fad7
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-03-25 09:31:39 +0100

    6.3 functionnal tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.infer_spec_type_from_file_location!
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
