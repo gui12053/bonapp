8346515d578271b15a24f9e5367c40719acb82b3
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-04-22 10:59:15 +0200

    6.10 Comments with AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  load_and_authorize_resource
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
@@ -15,11 +15,12 @@ class CommentsController < ApplicationController
        else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.js
        end
      end
    end
 
-   def destroy
+  def destroy
     @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
@@ -27,7 +28,7 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating)
-    end
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :rating)
+  end
 end
