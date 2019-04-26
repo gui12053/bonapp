67e81bb5546ba1cd74bd0d13b9072e456e2989ee
Guillaume Le Denmat <ledenmatg@gmail.com>
2019-04-26 15:40:48 +0200

    6.6 fixing user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,15 @@ class UserMailer < ApplicationMailer
     mail(from: email, to: 'guillaumeledenmat@hotmail.com', subject: "A new contact form message from #{name}")
   end
   def welcome(user)
-    @appname = "SiverWater"
+    @appname = "InAudible"
     mail(to: user.email,
     subject: "Welcome to #{@appname}!")
+  end
+  def order_placed(user, product)
+    @appname = "InAudible"
+    @product = product
+    @user = user
+    mail(to: user.email,
+    subject: "Your oder at #{@appname}")
   end
 end
